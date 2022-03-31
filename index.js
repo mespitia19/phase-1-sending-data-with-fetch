6081871def08de3a101cb272c705192e425078c8
MaximoEspitia <max19espitia@gmail.com>
2022-03-30 20:02:16 -0500

    commit tree 85486d258883bcdbe61f61428817a172af3d50b6

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    //fetch('http://localhost:3000/users')
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": userName,
+            "email": userEmail
+        })
+    };
+    return fetch("http://localhost:3000/users",configObj)
+    .then(res => res.json())
+    .then(body => addNewContact(body.id))
+
+    .catch(response => {
+        console.log(response)
+        document.body.append(response)
+    })
+    //document.body.append(message)
+    //console.log(response)
+    
+}
+function addNewContact(newID) {
+    document.getElementsByTagName("body")[0].innerHTML =newID;
+    
+
+}
+submitData("Name", "name@name.com")
